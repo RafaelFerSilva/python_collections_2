c5acb4e2ddadebd22e1ffad5d8b2996260ecf8f2
Rafael Fernandes da Silva <81975862+Rafael-Orbia@users.noreply.github.com>
2023-07-24 17:36:57 -0300

    Operações com conjuntos

--- 1 - conjuntos_sets.py
+++ 1 - conjuntos_sets.py
@@ -5,9 +5,25 @@ usuarios_machine_learning = {13, 23, 56, 42}
 # Conjuntos não tem ordem, index
 
 assistiram = usuarios_data_science.copy()
+
 # assistiram.extend(usuarios_machine_learning)
 
 # for usuario in set(assistiram):
 #     print(usuario)
 
-print( usuarios_data_science | usuarios_machine_learning)
+# Itens que são únicos entre os conjuntos
+print(usuarios_data_science | usuarios_machine_learning)
+
+# Itens que estão em ambos conjuntos
+print(usuarios_data_science & usuarios_machine_learning)
+
+# Está no primeiro mas não esta no segundo
+print(usuarios_data_science - usuarios_machine_learning)
+
+
+fez_ds_mas_nao_fez_ml = usuarios_data_science - usuarios_machine_learning
+print(15 in fez_ds_mas_nao_fez_ml)
+print(23 in fez_ds_mas_nao_fez_ml)
+
+# Está em um dos conjuntos mas não nos dois
+print(usuarios_data_science ^ usuarios_machine_learning)
